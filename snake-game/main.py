95af9ad51ee6c32b6a01f11e2f77b62fefd9acdb
dileepthutta <thuttadileep@gmail.com>
2025-06-17 21:19:37 +0530

    converted into classes

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle,Screen
+from snake import Snake
 import time 
 
 screen = Screen()
@@ -7,29 +8,12 @@ screen.bgcolor("black")
 screen.title("My Snake Game ")
 screen.tracer(0) # Untill we use (update) we screen will be blank.
 
-#Creating a Snake body.
-starting_position = [(0,0),(-20,0),(-40,0)]
+snake = Snake()
 
-segments = []
-for position in starting_position:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()    #Avoid to draw on Screen.
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-#Move the snake.
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1) #Adds a 0.1 sec delay for each pieace.
     
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x,new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
-
-
+    snake.move()
 screen.exitonclick()

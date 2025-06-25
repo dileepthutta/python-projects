a133c47a1e71f81d4a766872ec707c3458872f64
dileepthutta <thuttadileep@gmail.com>
2025-06-25 18:30:51 +0530

    Functionality Added

--- US-States-Game/main.py
+++ US-States-Game/main.py
@@ -1,4 +1,8 @@
 import turtle
+from turtle import Turtle
+
+import pandas as pd
+from pandas import read_csv
 
 screen = turtle.Screen()
 screen.title('U.S States Game')
@@ -7,14 +11,30 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-#To Create a pop-up box and User ask user-input.
-answer_state = screen.textinput(title="Guess the State",prompt="What's another state's name? ")
-print(answer_state)
+data = read_csv('50_states.csv')
+all_states = data.state.to_list()
+
+guessed_state = []
+
+while len(guessed_state) < 50:
+    # To Create a pop-up box and User ask user-input.
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct",
+                                    prompt="What's another state's name? ").title()
+    #Check's if the user-state is correct || not.
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(),state_data.y.item())
+        t.write(answer_state)
+screen.exitonclick()
 
 
 
 #Function to get the co-ordinate of x,y values of  the image onclick.
-def get_mouse_click_coor(x , y):
-    print(x,y)
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
+# def get_mouse_click_coor(x , y):
+#     print(x,y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()

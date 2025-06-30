debdd75a91ddc86d08927641c60b1ba75bbced5f
dileepthutta <thuttadileep@gmail.com>
2025-06-30 20:21:37 +0530

    Handles Error's

--- NATO-Alphabet/main.py
+++ NATO-Alphabet/main.py
@@ -5,7 +5,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 #TO Create a dictionary for the data:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-#To Create a list of code words from the word that the user inputs.
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only alphabets are allowed.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()

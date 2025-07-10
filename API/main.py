fe6d3b68424ed8ec862bf31f6e31b23c3251072b
dileepthutta <thuttadileep@gmail.com>
2025-07-10 20:49:10 +0530

    Check weather-condition

--- API/main.py
+++ API/main.py
@@ -8,6 +8,7 @@ weather_params = {
     "lat": "17.688112",
     "lon": "83.213120",
     "appid":api_key,
+    "cnt" :4
 }
 
 response = requests.get(url=own_endpoint,params=weather_params)
@@ -16,4 +17,19 @@ response.raise_for_status()
 
 weather_data = response.json()
 
-print(weather_data)
+# weather_list = weather_data['list'][0]['weather'][0]['id']
+
+'''
+TO check the weather condition based on id.
+To check if id (less than 700) return user to bring the Umbrella.
+'''
+
+will_rain = False
+
+for hour_data in weather_data['list']:
+    condition_code = hour_data['weather'][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")

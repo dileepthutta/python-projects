6a17b7b6eb7e2ab92b11d06c92a558086e940e94
dileepthutta <thuttadileep@gmail.com>
2025-07-18 21:30:53 +0530

    # Create a graph definition

--- Daily-Track/main.py
+++ Daily-Track/main.py
@@ -1,15 +1,44 @@
+from http.client import responses
+
 import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
-# Parameters to create a Account in Pixela using POST request.
+USER_NAME = ""
+TOKEN = ""
+
+# 1) Parameters to create a Account in Pixela using POST request.
 user_params = {
-    "token" : "OUR_OWN_TOKEN",
-    "username" : "USER_NAME",
+    "token" : TOKEN,
+    "username" : USER_NAME,
     "agreeTermsOfService" : "yes",
     "notMinor" : "yes"
 }
+# Create your user account
+# response = requests.post(url=pixela_endpoint,json=user_params)
 
-response = requests.post(url=pixela_endpoint,json=user_params)
 
-print(response.text)
+
+# Create a graph definition.
+graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
+
+# Json data to make a post request at graph_en_point.
+# Create a graph definition
+
+graph_config = {
+    "id" : "graph1",
+    "name" : "Cycling Graph",
+    "unit" : "Km",
+    "type" : "float",
+    "color" : "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN" : TOKEN
+}
+
+response = requests.post(
+    url=graph_endpoint,
+    json=graph_config,
+    headers=headers
+)

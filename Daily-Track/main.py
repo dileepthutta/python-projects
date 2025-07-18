f2baaa6289bc44470e5df8871900c202381d9931
dileepthutta <thuttadileep@gmail.com>
2025-07-18 21:52:35 +0530

    Add a pixel to the graph

--- Daily-Track/main.py
+++ Daily-Track/main.py
@@ -6,8 +6,9 @@ pixela_endpoint = "https://pixe.la/v1/users"
 
 USER_NAME = ""
 TOKEN = ""
+GRAPHID = "graph1"
 
-# 1) Parameters to create a Account in Pixela using POST request.
+# Step 1 ) Parameters to create a Account in Pixela using POST request.
 user_params = {
     "token" : TOKEN,
     "username" : USER_NAME,
@@ -19,7 +20,7 @@ user_params = {
 
 
 
-# Create a graph definition.
+# Step 2) Create a graph definition.
 graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
 
 # Json data to make a post request at graph_en_point.
@@ -37,8 +38,24 @@ headers = {
     "X-USER-TOKEN" : TOKEN
 }
 
+# response = requests.post(
+#     url=graph_endpoint,
+#     json=graph_config,
+#     headers=headers
+# )
+
+# Step3 Add a pixel into the Graph.
+
+post_pixel_end_point = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPHID}"
+
+pixel_config = {
+    "date" : "20250718",
+    "quantity" : "10.5"
+}
+
 response = requests.post(
-    url=graph_endpoint,
-    json=graph_config,
+    url=post_pixel_end_point,
+    json=pixel_config,
     headers=headers
 )
+print(response.text)

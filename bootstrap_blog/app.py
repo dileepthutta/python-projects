26ef5838c783bebee72a7311eed355b05c504257
dileepthutta <thuttadileep@gmail.com>
2025-08-25 21:51:31 +0530

    Dynamic data from the API

--- bootstrap_blog/app.py
+++ bootstrap_blog/app.py
@@ -2,10 +2,14 @@ from flask import Flask,render_template
 import requests
 
 app = Flask(__name__)
+# API URL || To use the data.
+API_URL = "https://api.npoint.io/bd8938422b0310f65538"
+response = requests.get(API_URL)
+data = response.json()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",data=data)
 
 @app.route("/about")
 def about():

720ba64f2ba4b632f3b7fee9dd5d1a0f2b6870d4
dileepthutta <thuttadileep@gmail.com>
2025-08-27 13:48:22 +0530

    contact with fully functional

--- bootstrap_blog/app.py
+++ bootstrap_blog/app.py
@@ -7,25 +7,20 @@ API_URL = "https://api.npoint.io/9a26c380138981bcfd89"
 response = requests.get(API_URL)
 data = response.json()
 
+
+# Home Route.
 @app.route("/")
 def home():
     return render_template("index.html",data=data)
 
+#About Route.
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+#Contact route with functionality
+@app.route("/contact",methods=["POST","GET"])
 def contact():
-    return render_template("contact.html")
-
-@app.route("/post/<int:num>")
-def post(num):
-    requested_post = next((posts for posts in data if posts["id"] == num), None)
-    return render_template("post.html",post=requested_post)
-
-@app.route("/form-entry",methods=["POST","GET"])
-def receive_data():
     if request.method == "POST":
         name = request.form.get('name')
         email = request.form.get('email')
@@ -39,9 +34,17 @@ def receive_data():
         }
         for item in data.values():
             print(item)
-        return "<h1>Login successfully Done</h1>"
+        return render_template("contact.html",msg_sent=True)
     else:
-        pass
+        return render_template("contact.html",msg_sent=False)
+
+
+# Post route
+@app.route("/post/<int:num>")
+def post(num):
+    requested_post = next((posts for posts in data if posts["id"] == num), None)
+    return render_template("post.html",post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
